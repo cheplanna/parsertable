b5d8e3362555e5e7c0c875b0fe95459dd2d6f709
cheplanna <72358616+cheplanna@users.noreply.github.com>
2020-12-01 00:02:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import csv
 import pickle
 from datetime import datetime
 from tabulate import tabulate
+import pandas
+
 
 d1=datetime(2005,7,14,4,2)
 d2=datetime(2006,7,14)
@@ -21,90 +23,25 @@ def load_table(file, type="csv"):
     if type == "pickle":  # Считываем таблицу используя pickle
         dictionary = pickle.load(file)
     elif type == "csv":  # Считываем таблицу используя csv
-        file_reader = csv.reader(file, delimiter=",")  # преобразуем файл в лист листов
-        table_key_dictionary = {}  # Создаем словарь атрибутов таблицы и записываем их номер
-        # Счетчик для подсчета количества строк
-        lines_count = 0  # Считаем номер строки
-        # Считывание данных из CSV файла
-        for row in file_reader:  # Проходимся по строкам таблицы
-            if lines_count == 0:  # В первой строк находяться атрибуты, создаем ключи в словаре
-                key_count = 0
-                for i in row:
-                    dictionary[i] = []
-                    table_key_dictionary[key_count] = i
-                    key_count += 1
-            else:
-                key_count = 0
-                for i in row:
-                    dictionary.get(table_key_dictionary.get(key_count)).append(
-                        i)  # Записываем нужный столбик нужный элемент
-                    key_count += 1
-            lines_count += 1
+        dictionary = pandas.read_csv(file)
+        print(dictionary)
     else:
         return -1
     return dictionary
 
 def save_table(data, type='txt'):
     if type == 'csv':
-        temp_table = []
-        field_names = data.keys()
-        num_of_colums = 0
-        for i in field_names:
-            num_of_colums = max(num_of_colums, len(data[i]))
-        for i in range(0, num_of_colums):
-            values = dict.fromkeys(field_names)
-            for j in field_names:
-                if i < len(data[j]):
-                    values[j] = data[j][i]
-            temp_table.append(values)
-        print(temp_table)
-        with open('NewFile2' + '.csv', 'a+') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=field_names)
-            writer.writeheader()
-            writer.writerows(temp_table)
-
+        table = pandas.read_csv('NewFile2.csv')
+        table.to_csv('Filek.csv')
     elif type == 'pickle':
-        with open('NewFile1' + '.pickle', 'wb') as f:
+        with open('Filek' + '.pickle', 'wb') as f:
             pickle.dump(data, f)
         f.close()
 
     elif type == 'txt':
-        with open('NewFile1' + '.txt', 'w') as f:
+        with open('Filek' + '.txt', 'w') as f:
             data = temp_data
-            len_of_col = {}  # СЛОВАРЬ ДЛЯ ХРАНЕНИЯ ДЛИНЫ СТОЛБЦОВ
-            max_l = 0
-            for i in data:  # ПЕРЕБОР СЛОВАРЯ ПО ВСЕМ СЛОВАМ И ЗНАЧЕНИЯМ
-                if len(i) > max_l:  # НАХОЖДЕНИЕ САМОГО ДЛИННОГО СЛОВА
-                    max_l = len(i)
-                for j in data[i]:
-                    if len(str(j)) > max_l:
-                        max_l = len(str(j))
-                len_of_col[i] = max_l  # ЗАПОЛНЕНИЯ СЛОВАРЯ МАКСИМАЛЬНЫМИ ДЛИНАМИ
-
-            field_names_len = {}
-            for i in len_of_col:  # ФОРМИРОВАНИЕ РОВНЫХ СТОЛБЦОВ
-                temp_key = i
-                if len(i) < len_of_col[i]:
-                    while len(i) < len_of_col[temp_key]:
-                        i = i + ' '
-                print(i + '|', file=f, end="")
-                field_names_len[i] = len_of_col[temp_key]
-            print(file=f)
-
-            num_str = 0
-            while True:
-                try:
-                    for i in data:  # ДОБАВЛЕНИЕ ПРОБЕЛОВ ДЛЯ РОВНЫХ СТОЛБЦОВ
-                        temp = str(data[i][num_str])
-                        if len(temp) < len_of_col[i]:
-                            while len(temp) < len_of_col[i]:
-                                temp += ' '
-                        temp += '|'
-                        print(temp, file=f, end='')
-                    print(file=f)
-                    num_str += 1
-                except IndexError:
-                    break
+            f.write(tabulate(data, headers='keys', tablefmt="grid"))
         f.close()
 
 def set_values(values, column=''):
@@ -230,12 +167,3 @@ def get_column_types(by_number=True):
 def print_table():
     data = temp_data
     print(tabulate(data, headers='keys', tablefmt="grid"))
-
-
-
-load_table('Example', type="csv")
-save_table(temp_data, type='csv')
-
-print_table()
-new = get_values(3)
-print(new)
